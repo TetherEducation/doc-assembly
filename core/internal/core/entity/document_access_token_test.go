d317c449a54589e916d141eb45d100062093e84c
Reynaldo Endis Peña <reynaldo.endis@gmail.com>
2026-05-07 17:29:36 -0400

    refactor: polish read-only view contracts

--- core/internal/core/entity/document_access_token_test.go
+++ core/internal/core/entity/document_access_token_test.go
@@ -1,15 +1,13 @@
-package document
+package entity
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/rendis/doc-assembly/core/internal/core/entity"
 )
 
 func TestDocumentAccessToken_ViewOnlyType(t *testing.T) {
-	token := &entity.DocumentAccessToken{TokenType: entity.TokenTypeViewOnly}
+	token := &DocumentAccessToken{TokenType: TokenTypeViewOnly}
 
 	require.True(t, token.IsViewOnly())
 	require.False(t, token.IsSigning())
